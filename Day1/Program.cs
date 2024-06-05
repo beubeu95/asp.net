9e18591464848cf9fe681acbb8cac3b904fb6aab
beubeu95 <xuanjiong810@gmail.com>
2024-06-05 15:23:12 +0900

    asp.net start

---
+++ Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+
+bool boolVariable = true;
+    int intVariable = 10;
+    float floatVariable = 3.4f;
+    char charVariable = 'a';
+
+    Console.WriteLine (boolVariable);
+    Console.WriteLine (intVariable);
+    Console.WriteLine (floatVariable);
+    Console.WriteLine (charVariable);
+
+for(int i = 0; i<10; i++){
+    Console.WriteLine(i);
+}
